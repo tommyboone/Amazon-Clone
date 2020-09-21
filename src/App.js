91dda8ca2f94f405109a0819b976525a65c72062
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-09-21 13:33:27 -0500

    added Home and Product Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
 
       {/*Add Header Component */}
       {/*Add Body Component */}
