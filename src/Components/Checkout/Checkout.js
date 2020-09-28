a7f1474f22651dae46dba033a4c8f9d4ede03f69
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-09-28 11:53:01 -0500

    created Checkout and Subtotal Components

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Checkout.css";
+import Subtotal from "../Subtotal/Subtotal";
 
 function Checkout() {
   return (
@@ -14,7 +15,7 @@ function Checkout() {
         <h2 className="checkout__title">Your Shopping Cart</h2>
       </div>
       <div className="checkout__right">
-        <h2>Subtotal goes here:</h2>
+        <Subtotal />
       </div>
       {/* Basket item goes here*/}
     </div>
