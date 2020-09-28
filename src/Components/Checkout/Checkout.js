21ef70a0af977a3c68517a1d6b54361994e581b5
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-09-28 12:43:28 -0500

    added Products from cart to checkout page

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "../Subtotal/Subtotal";
+import CheckoutProduct from "../CheckoutProduct/CheckoutProduct";
+import { useStateValue } from "../../StateProvider";
 
 function Checkout() {
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -13,6 +16,15 @@ function Checkout() {
         />
 
         <h2 className="checkout__title">Your Shopping Cart</h2>
+        {basket.map((item) => (
+          <CheckoutProduct
+            id={item.id}
+            title={item.title}
+            image={item.image}
+            price={item.price}
+            rating={item.rating}
+          />
+        ))}
       </div>
       <div className="checkout__right">
         <Subtotal />
