a875d9bcae39dbdb4a7f236268a2e20f41a377d0
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-09-28 12:20:50 -0500

    basket subtotal rendering correctly

--- src/Components/Subtotal/Subtotal.js
+++ src/Components/Subtotal/Subtotal.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "../../StateProvider";
+import { getBasketTotal } from "../../reducer";
 function Subtotal() {
+  const [{ basket }] = useStateValue();
   return (
     <div className="subtotal">
       <CurrencyFormat
@@ -9,7 +12,7 @@ function Subtotal() {
           <>
             <p>
               {/* Part of the homework */}
-              Subtotal (0 items): <strong>{value}</strong>
+              Subtotal ({basket?.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -17,7 +20,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0} // Part of the homework
+        value={getBasketTotal(basket)} // Part of the homework
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
