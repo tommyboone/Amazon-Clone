d5441f0241118bf24e536b97aad7c8e6f39a6744
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-09-28 13:34:24 -0500

    removed header from login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
 import Checkout from "./Components/Checkout/Checkout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
@@ -9,12 +10,16 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
