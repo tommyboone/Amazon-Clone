b72039a254b0e657e7343e7af557fe9974c16d97
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-10-01 11:44:25 -0500

    style updates

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,6 +14,7 @@ function Home() {
 
       <div className="home__row">
         <Product
+          className="product"
           id="12321341"
           title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
           price={11.96}
@@ -21,6 +22,7 @@ function Home() {
           image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
         />
         <Product
+          className="product"
           id="49538094"
           title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
           price={239.0}
@@ -31,6 +33,7 @@ function Home() {
 
       <div className="home__row">
         <Product
+          className="product"
           id="4903850"
           title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
           price={199.99}
@@ -38,6 +41,7 @@ function Home() {
           image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
         />
         <Product
+          className="product"
           id="23445930"
           title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
           price={98.99}
@@ -45,6 +49,7 @@ function Home() {
           image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
         />
         <Product
+          className="product"
           id="3254354345"
           title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
           price={598.99}
@@ -55,6 +60,7 @@ function Home() {
 
       <div className="home__row">
         <Product
+          className="product"
           id="90829332"
           title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
           price={1094.98}
