c93fabbfbc217b0623c0610fed0bfda82df7a0ed
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-10-01 10:55:04 -0500

    deployed database to firebase

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: "http://localhost:5001/clone-368c5/us-central1/api",
-  // "http://localhost:5001/challenge-4b2b2/us-central1/api",
+  baseURL: "https://us-central1-clone-368c5.cloudfunctions.net/api",
 });
 
 export default instance;
